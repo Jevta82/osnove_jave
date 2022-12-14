e9805db73354bea9f79009df711e2dfe72e77543
Aleksandar Jevtic <jevta1982@gmail.com>
2022-12-14 08:05:43 +0100

    Zadaci za vezbanje 13.12.2022

---
+++ src/main/java/Domaci13_12_2022/VezbanjeDomaci6.java
@@ -0,0 +1,36 @@
+package Domaci13_12_2022;
+
+public class VezbanjeDomaci6 {
+    public static void main(String[] args) {
+        String zvezdice = "***********************************************";
+        String email = "jevta1982@gmail.com";
+        String password = "12345678";
+        String zvezdice2 = "*************************";
+
+        System.out.println(zvezdice);
+        System.out.println("*                                             * ");
+        System.out.println("*                Register                     * ");
+        System.out.println("*           ___________________________       * ");
+        System.out.println("* Email:    |" + email + "      |       * ");
+        System.out.println("*            " + zvezdice2 + "        * ");
+        System.out.println("*                                             * ");
+        System.out.println("*           ___________________________       * ");
+        System.out.println("* Password:|" + password + "                <>|       * ");
+        System.out.println("*            " + zvezdice2 + "        * ");
+        System.out.println("*                                             * ");
+        System.out.println("*                                             * ");
+        System.out.println("*  _                                          * ");
+        System.out.println("* | | I Agree                                 * ");
+        System.out.println("*                                             * ");
+        System.out.println("*                      _______________        * ");
+        System.out.println("*                     |               |       * ");
+        System.out.println("*                     |  Register     |       * ");
+        System.out.println("*                     |_______________|       * ");
+        System.out.println("*                                             * ");
+        System.out.println(zvezdice);
+
+
+
+
+    }
+}
