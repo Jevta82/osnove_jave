39a38cbcbd49da44bebf5de4ab38844885be8f70
Aleksandar Jevtic <jevta1982@gmail.com>
2022-12-18 14:02:44 +0100

    Domaci 16.12.2022

---
+++ src/main/java/Domaci16_12_2022/Domaci2DrguiNacin.java
@@ -0,0 +1,14 @@
+package Domaci16_12_2022;
+
+public class Domaci2DrguiNacin {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <=25; i++) {
+            System.out.print("-");
+            if (i%5==0){
+                System.out.println();
+            }
+
+        }
+    }
+}
